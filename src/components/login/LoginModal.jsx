45953e5971c4ca1149ccff93dd6d7ae3cd896256
ZeusSantiago <163061179+ZeusSantiago@users.noreply.github.com>
2024-09-23 14:49:30 +0800

    Login modals
    Added raw Login Modal

---
+++ src/components/login/LoginModal.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+
+
+    
+export const openModal = id=>{
+    const modal = document.getElementById(id);
+
+    modal.style.display = "block"
+};
+export const hideModal = id=>{
+    const modal = document.getElementById(id);
+
+    modal.style.display = "none"
+};
+const LoginModal = ({children, id}) => {
+    return (
+        <div id={id} className="hidden fixed top-0 letf-0 w-full h-full bg-gray-500 bg-opacity-70 z-10 overflow-hidden">
+            <div className="max-w-full max-h-full w-auto h-auto absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 rounded p-5 z-20 bg-slate-400">{children}
+
+            <button 
+                onClick={() => hideModal(id)} 
+                className="w-7 h-7 bg-slate-600 flex items-center justify-center absolute top-4 right-4 rounded text-2xl text-white z-40"> 
+                &times;
+            </button>
+
+            </div>
+        </div>
+    )
+}
+
+export default LoginModal;
