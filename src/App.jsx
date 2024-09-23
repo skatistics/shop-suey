ed860a48cf88e12612816e18d4cffde0b9f58671
ZeusSantiago <163061179+ZeusSantiago@users.noreply.github.com>
2024-09-23 14:49:30 +0800

    Login modals
    Added raw Login Modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import LoginModal from "./components/login/LoginModal";
+import TempPage from "./components/login/TempPage";
+
 function App(){
   return(
     <>
+    <TempPage/>
+    <LoginModal/>
     </>
   );
 }
