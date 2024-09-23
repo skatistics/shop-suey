b5e8b764325fc7514b45a9fec362afee9b5ca419
Kirlian Verus <127189354+Kyverus@users.noreply.github.com>
2024-09-23 14:29:49 +0800

    Added Product List
    Added Product Items
    Added Product List

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { useState, useEffect } from "react";
+import ProductList from "./components/products/ProductList";
+
 function App(){
+  const [products, setProducts] = useState([]);
+
+  useEffect(()=>{
+    fetch("https://api.escuelajs.co/api/v1/products").then(res=>res.json()).then(data=>setProducts(data));
+  },[products]);
+
   return(
-    <>
-    </>
+    <ProductList products={products}/>
   );
 }
 
--- src/components/products/ProductItems.jsx
+++
@@ -1,8 +0,0 @@
-function ProductItems(){
-    return(
-        <div></div>
-    );
-
-}
-
-export default ProductItems 
