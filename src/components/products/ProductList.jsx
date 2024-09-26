31e2b884cb563a332e88bf787dd3e12975d14753
Kirlian Verus <127189354+Kyverus@users.noreply.github.com>
2024-09-26 11:36:14 +0800

    Updated Product List
    Redesigned Product Items
    Added Product Dark Mode Responsive

--- src/components/products/ProductList.jsx
+++ src/components/products/ProductList.jsx
@@ -2,7 +2,7 @@ import ProductItem from "./ProductItem";
 
 function ProductList({ products }) {
   return (
-    <div className="md:container mx-auto grid grid-cols-4 gap-4 justify-center">
+    <div className="md:container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 justify-center">
       {products.map((product) => {
         return <ProductItem key={product.id} product={product} />;
       })}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  darkMode: 'class',
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
-    screens:{
-      xs:'375px',
-      sm:'640px',
-      md:'768px',
-      lg:'1024px',
-      xl:'1280px',
+    screens: {
+      xs: "375px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
     },
     extend: {
-      colors:{
-        'ct-light-olive-A3BCB6': '#A3BCB6',
-        'ct-light-greenleaf-39603D': '#39603D',
-        'ct-light-browngrey-3C403D': '#3C403D',
-        'ct-light-tanly-DADED4': '#DADED4',
-        'ct-dark-green-164A41': '#164A41',
-        'ct-dark-green-4D774E': '#4D774E',
-        'ct-dark-green-9DC88D': '#9DC88D',
-        'ct-dark-yellow-F1B24A': '#F1B24A',
+      colors: {
+        "ct-light-olive-A3BCB6": "#A3BCB6",
+        "ct-light-greenleaf-39603D": "#39603D",
+        "ct-light-browngrey-3C403D": "#3C403D",
+        "ct-light-tanly-DADED4": "#DADED4",
+        "ct-dark-green-164A41": "#164A41",
+        "ct-dark-green-4D774E": "#4D774E",
+        "ct-dark-green-9DC88D": "#9DC88D",
+        "ct-dark-yellow-F1B24A": "#F1B24A",
+        "ct-white-FEFCFF": "#FEFCFF",
+        "ct-black-19191A": "#19191A",
       },
     },
   },
-  plugins: [],
-}
-
-
-
+  plugins: [require("@tailwindcss/line-clamp")],
+};
