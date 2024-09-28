f68807e4dd0942c128adaec449b97c82d8c52ad9
seusuuu <zeussantiago9@gmail.com>
2024-09-28 14:55:23 +0800

    Cart update
    Remove button
    add minus item

--- src/components/modals/CartList.jsx
+++ src/components/modals/CartList.jsx
@@ -3,7 +3,7 @@ import CartListItem from "../CartListItem";
 function CartList() {
   return (
     <div>
-      <div className="h-[600px] fixed w-72 right-7 overflow-hidden hover:overflow-y-scroll transition-all hover:scrollbar-thin hover:scrollbar-webkit border-black border-2 top-[110px]">
+      <div className="h-[600px] fixed w-72 right-7 overflow-hidden hover:overflow-y-scroll transition-all hover:scrollbar-thin hover:scrollbar-webkit border-black border-2 top-[110px] hover:w-[303px] hover:right-[13px]">
         <div>
           <CartListItem />
           <CartListItem />
