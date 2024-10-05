cbc6917e2229e10e1851ff8fefc9e0b105c26381
skatistics <131618960+skatistics@users.noreply.github.com>
2024-10-05 14:58:05 +0800

    Added CategoryModalToggle function
    Added CategoryModalToggle function

---
+++ src/components/floating/FloatingCategoryToggle.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect } from "react";
+import {
+  closeCategoryModal,
+  toggleCategoryModal,
+} from "../modals/CategoryModal";
+
+function FloatingCategoryToggle() {
+  useEffect(() => {
+    const categoryToggle = document.getElementById("category-toggle");
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            categoryToggle.classList.add("hidden");
+            closeCategoryModal();
+          } else {
+            categoryToggle.classList.remove("hidden");
+          }
+        });
+      },
+      { threshold: 0 }
+    );
+
+    const bannerSection = document.getElementById("banner-section");
+    if (bannerSection) {
+      observer.observe(bannerSection);
+    }
+  }, []);
+
+  return (
+    <>
+      <button
+        id="category-toggle"
+        className="fixed top-0 left-4 hidden rounded-full p-2 text-ct-1F331F bg-ct-F2F7F2 dark:text-ct-D9E8D9 dark:bg-[#00ff3770] border-2 border-ct-base-67A770"
+        onClick={() => toggleCategoryModal()}
+      >
+        Categories
+      </button>
+    </>
+  );
+}
+
+export default FloatingCategoryToggle;
--- src/components/modals/CategoryModal.jsx
+++ src/components/modals/CategoryModal.jsx
@@ -1,8 +1,12 @@
-import { Modal } from "./CustomModal";
+import LeftSection from "../sections/LeftSection";
+import { Modal, openModal, hideModal } from "./CustomModal";
+import { useContext } from "react";
+import { ProductContext } from "../contexts/ProductContextProvider";
 
 export function toggleCategoryModal() {
   const categoryModal = document.getElementById("category-modal");
   const hidden = categoryModal.classList.contains("hidden");
+
   if (hidden) {
     openModal("category-modal");
   } else {
@@ -10,20 +14,18 @@ export function toggleCategoryModal() {
   }
 }
 
+export function closeCategoryModal() {
+  hideModal("category-modal");
+}
+
 function CategoryModal() {
+  const categories = useContext(ProductContext).categories;
   return (
-    <Modal className={"top[100px] left-0"} id="category-modal">
-      <div>
-        <a>Category</a>
-        <a>Category</a>
-        <a>Category</a>
-        <a>Category</a>
-        <a>Category</a>
-        <a>Category</a>
-        <a>Category</a>
-        <a>Category</a>
-      </div>
-    </Modal>
+    <div>
+      <Modal className={"size-[200px] top-[100px]"} id="category-modal">
+        <LeftSection categories={categories} />
+      </Modal>
+    </div>
   );
 }
 
