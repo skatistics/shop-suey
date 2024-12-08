34f5ab5285719cc1503a43505e78dff8bb33ec24
Kirlian Verus <127189354+Kyverus@users.noreply.github.com>
2024-12-08 08:23:10 +0800

    Redesigned LoginSignupModal
    Changed Google Icon
    Redesigned Modal Colors

--- src/components/modals/LoginSignupModal.jsx
+++ src/components/modals/LoginSignupModal.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { IoClose } from "react-icons/io5";
 import { Modal, openModal, hideModal, DisabledBGModal } from "./CustomModal";
-import { FaFacebook, FaGoogle } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
 
 export function openLoginSignupModal() {
   openModal("login-signup-modal");
@@ -23,22 +24,22 @@ export default function LoginSignupModal() {
     <DisabledBGModal
       id="login-signup-modal"
       className={
-        "top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 rounded p-5 bg-ct-45634F z-50"
+        "top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 rounded p-5 bg-ct-D9E8D9 dark:bg-ct-191819 text-ct-0B0D0C dark:text-ct-F2F7F2 z-50"
       }
     >
       <button
         onClick={() => closeLoginSignupModal()}
-        className="absolute top-4 right-4 w-7 h-7 rounded text-2xl text-white bg-slate-600 flex justify-center items-center"
+        className="absolute top-4 right-4 w-7 h-7 rounded text-2xl flex justify-center items-center"
       >
         <IoClose />
       </button>
       {modalState === "login" && (
         <div>
-          <h1 className="text-2xl font-semibold mb-7 text-white"> Login </h1>
+          <h1 className="text-2xl font-semibold mb-7"> Login </h1>
           <form className="w-[450px]">
             <div>
               <input
-                className="w-full py-2 px-4 rounded bg-gray-500 text-white mb-5 focus:outline-none"
+                className="w-full py-2 px-4 rounded bg-gray-200 dark:bg-gray-800 mb-5 focus:outline-none"
                 type="email"
                 placeholder="Enter user"
               />
@@ -46,14 +47,14 @@ export default function LoginSignupModal() {
 
             <div>
               <input
-                className="w-full py-2 px-4 rounded bg-gray-500 text-white mb-5 focus:outline-none"
+                className="w-full py-2 px-4 rounded bg-gray-200 dark:bg-gray-800 mb-5 focus:outline-none"
                 type="password"
                 placeholder="Password"
                 autoComplete="on"
               />
             </div>
           </form>
-          <button className="w-full text-white py-2 px-4 rounded text-[15px] bg-ct-dark-green-9DC88D hover:bg-ct-dark-green-4D774E">
+          <button className="w-full py-2 px-4 rounded text-[15px] bg-ct-8DB98E dark:bg-ct-45634F">
             Login to your account
           </button>
 
@@ -64,13 +65,15 @@ export default function LoginSignupModal() {
                 <FaFacebook className="size-[40px] text-[#1877F2] mr-6 hover:border-2 rounded-full" />
               </a>
               <a href="#">
-                <FaGoogle className="size-[40px] text-[#DB4437] hover:border-2 rounded-full" />
+                <FcGoogle className="size-[40px] hover:border-2 rounded-full" />
               </a>
             </div>
           </div>
-          <div className="text-center pt-4">Don't have an account yet?</div>
+          <div className="text-center pt-4 text-ct-base-67A770">
+            Don't have an account yet?
+          </div>
           <button
-            className="w-full text-ct-dark-green-9DC88D px-4 rounded text-[15px] bg-transparent hover:underline hover:text-ct-light-olive-A3BCB6 text-bold font-bold"
+            className="w-full text-ct-base-67A770 hover:text-ct-2F4C2F dark:hover:text-ct-A6C9A7  px-4 mt-2 rounded text-[15px] bg-transparent hover:underline text-bold font-bold"
             onClick={() => changeModalState("signup")}
           >
             Register
@@ -79,11 +82,11 @@ export default function LoginSignupModal() {
       )}
       {modalState === "signup" && (
         <div>
-          <h1 className="text-2xl font-semibold mb-7 text-white"> Sign Up </h1>
+          <h1 className="text-2xl font-semibold mb-7"> Sign Up </h1>
           <form className="w-[450px]">
             <div>
               <input
-                className="w-full py-2 px-4 rounded bg-gray-500 text-white mb-5 focus:outline-none"
+                className="w-full py-2 px-4 rounded bg-gray-200 dark:bg-gray-800 mb-5 focus:outline-none"
                 type="email"
                 placeholder="Enter user"
               />
@@ -91,7 +94,7 @@ export default function LoginSignupModal() {
 
             <div>
               <input
-                className="w-full py-2 px-4 rounded bg-gray-500 text-white mb-5 focus:outline-none"
+                className="w-full py-2 px-4 rounded bg-gray-200 dark:bg-gray-800 mb-5 focus:outline-none"
                 type="password"
                 placeholder="Password"
               />
@@ -110,7 +113,7 @@ export default function LoginSignupModal() {
               </a>
             </p>
           </div>
-          <button className="w-full text-white py-2 px-4 rounded text-[15px] bg-ct-dark-green-9DC88D hover:bg-ct-dark-green-4D774E">
+          <button className="w-full py-2 px-4 rounded text-[15px] bg-ct-8DB98E dark:bg-ct-45634F">
             Register your account
           </button>
           <div className="mt-4">
@@ -120,12 +123,12 @@ export default function LoginSignupModal() {
                 <FaFacebook className="size-[40px] text-[#1877F2] mr-6 hover:border-2 rounded-full" />
               </a>
               <a href="#">
-                <FaGoogle className="size-[40px] text-[#DB4437] hover:border-2 rounded-full" />
+                <FcGoogle className="size-[40px] text-[#DB4437] hover:border-2 rounded-full" />
               </a>
             </div>
           </div>
           <button
-            className="w-full text-ct-dark-green-9DC88D hover:text-ct-light-olive-A3BCB6 py-1 px-4 rounded text-[15px] bg-transparent hover:underline text-bold font-bold mt-4"
+            className="w-full text-ct-base-67A770 hover:text-ct-2F4C2F dark:hover:text-ct-A6C9A7 py-1 px-4 rounded text-[15px] bg-transparent hover:underline text-bold font-bold mt-4"
             onClick={() => changeModalState("login")}
           >
             Back to login
